310dbd3739c6aa2049263a509529630c602544b5
martinmourinoj-svg <martinmourinoj@gmail.com>
2026-02-05 18:11:00 +0100

    ver3

--- src/main/java/Empleado.java
+++ src/main/java/Empleado.java
@@ -7,4 +7,8 @@ public class Empleado {
     private String nombre;
     private int edad;
     private double sueldo;
+
+public void incrementarSueldo(int porcentajeIncremento){
+ this.sueldo+=sueldo*(1+porcentajeIncremento/100.0); 
+ } 
 }
